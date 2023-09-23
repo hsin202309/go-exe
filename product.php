cf37b408df18e968308c291e68f7dbd2201ebbee
hsin202309 <145846241+hsin202309@users.noreply.github.com>
2023-09-23 20:54:54 +0800

    Add files via upload

---
+++ product.php
@@ -0,0 +1,332 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>EXE卓品國際 - 口腔護理</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<meta name="description" content="「口腔生活體驗館」是以販售口腔清潔與治療相關之產品的單位，館內產品均由專科牙醫師親自研發試驗的國家認證標準醫療衛生產品，同時結合生活美學的概念進行產品設計。" >
+<meta name="keywords" content="EXE卓品國際,牙醫事業、商品、整合行銷部、創意投資" />
+<meta name="author" content="EXE卓品國際" />
+<meta name="copyright" content="EXE卓品國際" />
+<link href="css/reset.css" rel="stylesheet" type="text/css">
+<link href="css/all.css" rel="stylesheet" type="text/css">
+<link href="css/index.css" rel="stylesheet" type="text/css">
+<link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="vendors/flexslider/flexslider.css" rel="stylesheet" type="text/css">
+<link href="vendors/slick/slick.css" rel="stylesheet" type="text/css">
+<link href="vendors/slick/slick-theme.css" rel="stylesheet" type="text/css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css" />
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+<script src="js/smooth-scroll.min.js"></script>
+<script src="js/all.js"></script>
+</head>
+
+<body>
+<div class="pwrap">
+<div class="setting product ">
+	<div class="ptitle">
+        <h2><span>PRODUCTS</span><br>口腔護理</h2>
+        
+        <div class="boxwrap">
+        <a href="dental.php" title="牙醫事業部" class="pbox bluebox"></a>
+        
+        <div class="pbox greenbox"><div class="darrow"></div></div>
+        
+        <a href="marketing.php" title="整合行銷部" class="pbox redbox"></a>
+        <a href="venture.php" title="創意投資部" class="pbox yellowbox"></a>
+        </div>
+        <div class="clean"></div>
+    </div>
+    
+    <div class="pcontent">
+    	<div class="plogo">
+        	<img src="images/Sexe02.png" alt=""/>
+            <h3>口腔護理</h3>
+        </div>	
+        
+        <p>將美學設計融入口腔醫療保健用品是我們的宗旨，
+不只讓病人健康，更能輕鬆享受美學的生活體驗。</p>
+		<br>
+		<p>「口腔生活體驗館」是以販售口腔清潔與治療相關之產品的單位，館內產品均由專科牙醫師親自研發試驗的國家認證標準醫療衛生產品，同時結合生活美學的概念進行產品設計。</p>
+        <iframe width="100%" height="500" src="https://youtube.com/embed/Q1Md_KISykA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe></div>
+        <div class="clean">&nbsp;</div>
+        <div class="plist">
+        <div class="pcap"><a href="https://www.facebook.com/EXEhr8794/" target="_blank"><img src="http://go-exe.com/images/dicon01.png" alt="" width="177" height="177" /><br /> 進入口腔生活體驗館</a></div>
+        <div class="pcap ptop"><a href="https://line.me/R/ti/p/%40fwa6946v" target="_blank"><img src="http://go-exe.com/images/productqrcode.jpg" alt="" width="140" height="140" /><br /> 從齒更美好LINE</a></div>
+        <div class="pcap"><a href="#loc" data-scroll="" data-options=""><img src="http://go-exe.com/images/dicon03.png" alt="" width="175" height="175" /><br /> 販售據點</a></div>
+
+    </div>
+   
+    <div class="clean"></div>
+</div>
+
+<?php include 'nav.php';?>
+
+<div class="setting product" id="loc">
+    <div class="mheader">AS FEATURED IN</div>
+    <br><br>
+		<div class="pmap"><br>
+					<div class="slider autoplay">
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://thenycjournal.com/dr-yihua-huang/" target="_blank"><img src="images/news logo4.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://www.forbes.com/" target="_blank"><img src="images/news logo6.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="#" target="_blank"><img src="images/news logo7.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="#" target="_blank"><img src="images/news logo19.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://healthmedia.com.tw/main_detail.php?id=58685" target="_blank"><img src="images/news logo3.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://today.line.me/tw/v2/article/0MjRqLp" target="_blank"><img src="images/news logo5.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://today.line.me/hk/v2/article/XYe7xDk" target="_blank"><img src="images/news logo23.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://tw.news.yahoo.com/%E9%BB%83%E8%96%8F%E8%8F%AF%E9%86%AB%E5%B8%AB%E5%8F%97%E7%B4%90%E7%B4%84%E6%97%A5%E5%A0%B1%E9%9D%92%E7%9D%9E-%E7%99%BB%E5%85%A8%E7%90%8350%E4%BD%8D%E4%BC%81%E6%A5%AD%E9%A0%98%E8%A2%96%E6%A8%99%E7%AB%BF-020146283.html" target="_blank"><img src="images/news logo15.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://hk.news.yahoo.com/%E9%BB%83%E8%96%8F%E8%8F%AF%E9%86%AB%E5%B8%AB%E5%8F%97%E7%B4%90%E7%B4%84%E6%97%A5%E5%A0%B1%E9%9D%92%E7%9D%9E-%E7%99%BB%E5%85%A8%E7%90%8350%E4%BD%8D%E4%BC%81%E6%A5%AD%E9%A0%98%E8%A2%96%E6%A8%99%E7%AB%BF-020146283.html" target="_blank"><img src="images/news logo13.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://tw.sports.yahoo.com/news/%E9%BB%83%E8%96%8F%E8%8F%AF%E9%86%AB%E5%B8%AB%E5%8F%97%E7%B4%90%E7%B4%84%E6%97%A5%E5%A0%B1%E9%9D%92%E7%9D%9E-%E7%99%BB%E5%85%A8%E7%90%8350%E4%BD%8D%E4%BC%81%E6%A5%AD%E9%A0%98%E8%A2%96%E6%A8%99%E7%AB%BF-020146283.html" target="_blank"><img src="images/news logo14.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://style.yahoo.com.tw/news/%E9%BB%83%E8%96%8F%E8%8F%AF%E9%86%AB%E5%B8%AB%E5%8F%97%E7%B4%90%E7%B4%84%E6%97%A5%E5%A0%B1%E9%9D%92%E7%9D%9E-%E7%99%BB%E5%85%A8%E7%90%8350%E4%BD%8D%E4%BC%81%E6%A5%AD%E9%A0%98%E8%A2%96%E6%A8%99%E7%AB%BF-020146283.html" target="_blank"><img src="images/news logo11.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://today.line.me/tw/v2/article/0MjRqLp" target="_blank"><img src="images/news logo9.png" alt=""></a></div>
+						</div>https://m.match.net.tw/pc/life/health/20230607/7390122
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://m.life.tw/?app=view&no=1987744" target="_blank"><img src="images/news logo18.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://news.pchome.com.tw/healthcare/healthmedia/20230607/index-68610330699999281012.html" target="_blank"><img src="images/news logo17.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://n.yam.com/Article/20230607313669" target="_blank"><img src="images/news logo20.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://healthmedia.com.tw/blog/personal_news_detail.php?id=3950" target="_blank"><img src="images/news logo22.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="http://www.my-formosa.com/DOC_196106.htm" target="_blank"><img src="images/news logo16.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://www.4gtv.tv/article/2023060710000006" target="_blank"><img src="images/news logo19.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://www.beanfun.com/articles/detail/1666266173185986560?country=tw&site=281" target="_blank"><img src="images/news logo10.png" alt=""></a></div>
+						</div>
+						<div>
+							<div class="aspectration" data-ratio="5:3"><a href="https://tw.stock.yahoo.com/news/%E9%BB%83%E8%96%8F%E8%8F%AF%E9%86%AB%E5%B8%AB%E5%8F%97%E7%B4%90%E7%B4%84%E6%97%A5%E5%A0%B1%E9%9D%92%E7%9D%9E-%E7%99%BB%E5%85%A8%E7%90%8350%E4%BD%8D%E4%BC%81%E6%A5%AD%E9%A0%98%E8%A2%96%E6%A8%99%E7%AB%BF-020146283.html" target="_blank"><img src="images/news logo8.png" alt=""></a></div>
+						</div>					
+					</div>
+		</div>
+</div>
+<div class="setting plocation" id="loc">
+    <div class="mheader">REWARD</div><br><br><br><br>
+		<div class="pmap">
+			<div class="slider slider-nav4">
+				<div>
+					<div class="aspectration" data-ratio="5:3"><img src="images/REWARD08.png" alt=""></div>
+				</div>
+				<div>
+					<div class="aspectration" data-ratio="5:3"><img src="images/REWARD06.png" alt=""></div>
+				</div>
+				<div>
+					<div class="aspectration" data-ratio="5:3"><img src="images/REWARD07.png" alt=""></div>
+				</div>
+				<div>
+					<div class="aspectration" data-ratio="5:3"><img src="images/REWARD05.png" alt=""></div>
+				</div>
+				<div>
+					<div class="aspectration" data-ratio="5:3"><img src="images/REWARD04.png" alt=""></div>
+				</div><br><br>				
+		</div>
+		</div>
+</div>
+
+
+
+<div class="setting plocation" id="loc">
+	<div class="mheader">販售據點</div><br><br>
+	<div class="pmap">
+		<div class="slider slider-nav2">
+			<div>
+				<div class="aspectration" data-ratio="5:3"><a href="https://www.exebrush.com" target="_blank"><img src="images/shop02.png" alt=""></a></div>
+			</div>
+			<div>
+				<div class="aspectration" data-ratio="5:3"><a href="https://pinkoi.com/store/exetooth" target="_blank"><img src="images/shop04.png" alt=""></a></div>
+			</div>
+			<div>
+				<div class="aspectration" data-ratio="5:3"><a href="#"><img src="images/shop03.png" alt=""></a></div>
+			</div>				
+		</div>
+		<div class="slider slider-nav3">
+			<div>
+				<div class="aspectration" data-ratio="5:3"><a href="#"><img src="images/shop01.png" alt=""></a></div>
+			</div>
+			<div>
+				<div class="aspectration" data-ratio="5:3"><a href="#"><img src="images/shop05.png" alt=""></a></div>
+			</div>				
+		</div>
+	        <div class="mmap">
+        	<div class="mleft">
+            	<span style="color:#85c59d;">Zhubei 竹北</span><br>
+                口腔生活體驗館
+                <hr style="align:center;">
+                <p>
+                藝安牙醫<br>
+                302 新竹縣竹市光明六路東二段242號<br>
+                0800-800-812<br>
+                </p>
+            </div>
+            <div class="mright">
+            	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3621.4753594783556!2d121.03238231500197!3d24.813412284076257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34683701c034ee7d%3A0x291aa320139ef825!2zMzAy5paw56u557ij56u55YyX5biC5YWJ5piO5YWt6Lev5p2x5LqM5q61MjQy6Jmf!5e0!3m2!1szh-TW!2stw!4v1478075047121" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="mmap">
+        	<div class="mleft">
+            	<span style="color:#85c59d;">Taipei 台北 </span><br>
+            
+                
+                
+               口腔生活體驗館
+                <hr style="align:center;">
+                <p>EXE國際植牙中心<br>
+                105 台北市松山區復興北路73號2F<br>
+                0800-800-812<br>
+                </p>
+            </div>
+            <div class="mright">
+            	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.503393362356!2d121.5422163150067!3d25.05092188396445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abddbe1879db%3A0x710c747e43fba6b7!2zMTA15Y-w5YyX5biC5p2-5bGx5Y2A5b6p6IiI5YyX6LevNzPomZ8!5e0!3m2!1szh-TW!2stw!4v1485252575305" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="clean"></div>
+</div>
+</div>
+	<img src="images/top_btn.png" class="top_btn" alt="" />
+
+<?php include 'footer.php';?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="vendors/slick/slick.min.js"></script>
+<script src="js/fastclick.js"></script>
+<script src="js/smooth-scroll.min.js"></script>
+<script src="vendors/flexslider/jquery.flexslider.js"></script>
+<script src="js/all.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.js"></script>
+
+<script>
+smoothScroll.init({
+    selector: '[data-scroll]', 
+    selectorHeader: null, 
+    speed: 1000, 
+    easing: 'easeInOutCubic', 
+    offset: 0
+});
+
+$(function () {
+			window.FastClick.attach(document.body);
+		});
+
+		var flexslider = $('.flexslider');
+		if (flexslider.length) {
+			flexslider.flexslider({
+				animation: "slide",
+				controlNav: false,
+			});
+		}
+
+		smoothScroll.init({
+			selector: '[data-scroll]',
+			selectorHeader: null,
+			speed: 1000,
+			easing: 'easeInOutCubic',
+			offset: 0
+		});
+
+		$(document).ready(function () {
+			$('.autoplay').slick({
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				autoplay: true,
+				autoplayspeed: 1000,
+				dots: false,
+				centerMode: false,
+				centerPadding: '40px',
+				focusOnSelect: true,
+			});
+
+			$('.slider-for2').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: 0,
+				fade: true,
+				asNavFor: '.slider-nav2'
+			});
+
+			$('.slider-nav2').slick({
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				asNavFor: '.slider-for2',
+				dots: false,
+				centerMode: false,
+				centerPadding: '40px',
+				focusOnSelect: true
+			});
+			$('.slider-for3').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: 0,
+				fade: true,
+				asNavFor: '.slider-nav3'
+			});
+
+			$('.slider-nav3').slick({
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				asNavFor: '.slider-for3',
+				dots: false,
+				centerMode: false,
+				centerPadding: '40px',
+				focusOnSelect: true
+			});
+			$('.slider-for4').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: 0,
+				fade: true,
+				asNavFor: '.slider-nav4'
+			});
+
+			$('.slider-nav4').slick({
+				slidesToShow: 5,
+				slidesToScroll: 1,
+				asNavFor: '.slider-for4',
+				dots: false,
+				centerMode: false,
+				centerPadding: '40px',
+				focusOnSelect: true
+			});
+		});
+</script>
+</body>
+</html>
